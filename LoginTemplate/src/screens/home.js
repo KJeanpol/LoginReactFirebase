c71041f0e1801ef1de0fb29fc4913d232d6cb108
KJeanpol <ce.kalvarado@gmail.com>
2019-10-31 09:13:46 -0600

    Add Crud Firebase Example

--- LoginTemplate/src/screens/home.js
+++ LoginTemplate/src/screens/home.js
@@ -1,40 +1,80 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ToastAndroid
+} from "react-native";
 import * as firebase from "firebase";
 
 export default class HomeScreen extends React.Component {
-    state = {
-        email: "",
-        displayName: ""
-    };
+  state = {
+    email: "",
+    displayName: "",
+    name: ""
+  };
 
-    componentDidMount() {
-        const { email, displayName } = firebase.auth().currentUser;
+  componentDidMount() {
+    const { email, displayName } = firebase.auth().currentUser;
 
-        this.setState({ email, displayName });
-    }
+    this.setState({ email, displayName });
+  }
 
-    signOutUser = () => {
-        firebase.auth().signOut();
-    };
+  signOutUser = () => {
+    firebase.auth().signOut();
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Hi {this.state.email}!</Text>
+  _insert() {
+    firebase
+      .database()
+      .ref("users/" + this.state.displayName)
+      .set({
+        name: this.state.displayName
+      });
+    ToastAndroid.show("HOLLAAA", ToastAndroid.SHORT);
+  }
 
-                <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
-                    <Text>Logout</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+  _get() {
+    setTimeout(() => {
+      firebase
+        .database()
+        .ref("users/")
+        .on("value", data => {
+          console.log(data.toJSON());
+          ToastAndroid.show(JSON.stringify(data.toJSON()), ToastAndroid.SHORT);
+        });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi {this.state.email}!</Text>
+
+        <TouchableOpacity
+          style={{ marginTop: 32 }}
+          onPress={() => this._insert()}
+        >
+          <Text>INSERT</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ marginTop: 32 }} onPress={() => this._get()}>
+          <Text>GET</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
--- LoginTemplate/src/screens/loading.js
+++ LoginTemplate/src/screens/loading.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import * as firebase from "firebase";
 
 export default class LoadingScreen extends React.Component {
+
+    /*Este método solo se ejecuta justo después de que el componente haya sido montado en el DOM. 
+    Es el método perfecto para integrar librerias de terceros (plugins jquery), 
+    realizar alguna petición ajax ó establecer algún timer de tipo setTimeout ó setInterval*/
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             this.props.navigation.navigate(user ? "App" : "Auth");
